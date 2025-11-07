5f8364bbd0f3dea189b67ba09b36834fb75f3aad
Estelle Haubois <haubois.estelle@gmail.com>
2025-11-07 14:27:37 +0100

    style(homepage): ajout du style sur ma homepage

--- frontend/src/pages/homepage/Homepage.tsx
+++ frontend/src/pages/homepage/Homepage.tsx
@@ -1,32 +1,42 @@
-// page d'accueil / homepage
+import { useNavigate } from "react-router";
+import logo from "../../assets/logo.png"
+import './homepage.css'
 
-import { Link } from "react-router";
 
 export default function Homepage() {
+  const navigate = useNavigate();
+
   return (
-     <main className="main-content">
-      <h1>Bienvenue sur Axone</h1>
-      <p>
-        Partagez et découvrez des extraits de code utiles pour vos projets.
-      </p>
+     <main className="main-content homepage-container">
+        <section className="hero-section">
+          <h1>Bienvenue sur </h1>
+          <img src={logo} alt="Bannière Axone" /> 
+        </section>
 
-      {/* Lien vers la liste des snippets (à créer ensuite) */}
-      <Link to="/snippets">
-        Voir les snippets →
-      </Link>
+        <section className="presentation-section">
+        <h2>Présentation</h2>
+          <p>
+            Partagez et découvrez des extraits de code utiles pour vos projets.
+          </p>
+        </section> 
 
-      {/* Section "Derniers snippets" (mockée pour l'instant) */}
-      <div>
-        <h2>Derniers snippets ajoutés</h2>
-        <div>
-          <pre>
-            {`// Exemple de snippet
-function greet(name) {
-  return \`Hello, \${name}!\`;
-}`}
-          </pre>
+        <section className="cta-section">
+        <p>Pour continuer, veuillez vous inscrire ou vous identifier.</p>
+        <div className="cta-buttons">
+          <button 
+            className="btn btn--primary" 
+            onClick={() => navigate('/inscription')}
+          >
+            Inscription
+          </button> 
+          <button 
+            className="btn btn--secondary" 
+            onClick={() => navigate('/connexion')}
+          >
+            Connexion
+          </button>
         </div>
-      </div>
+      </section>
     </main>
   );
 }
